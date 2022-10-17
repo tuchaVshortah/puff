c6afcdd31d35656128a1fceec07187b1e47aa2f5
tuchaVshortah <nukenbai190104@gmail.com>
2022-10-17 14:50:05 +0600

    add more functionality + bug fixes

--- puff/main.py
+++ puff/main.py
@@ -8,11 +8,11 @@ def main():
     parser = argparse.ArgumentParser(prog="puff", description="Yet another subdomain enumeration tool")
     
     parser.add_argument(
-        "domains", 
+        "domain", 
         help="Specify domain to enumerate",
         default=[],
         type=str,
-        nargs="+"
+        nargs=1
     )
 
     group = parser.add_mutually_exclusive_group()
@@ -50,26 +50,44 @@ def main():
 
     args = parser.parse_args()
 
-    print(args.quiet)
     client = Client("api_key")
+    domain = args.domain[0]
 
     if(args.json == True):
-        for domain in args.domains:
-            response = client.get_raw(domain, output_format=Client.JSON_FORMAT)
-            response_data = json.loads(response)
-            pretty_response = json.dumps(response_data, indent=2)
-            print(pretty_response)
-            if(args.file is not None):
-                args.file.write(pretty_response)
+        response = client.get_raw(domain, output_format=Client.JSON_FORMAT)
+        response_data = json.loads(response)
+        pretty_response = json.dumps(response_data, indent=2)
+        print(pretty_response)
+
+        if(args.file is not None):
+            args.file.write(pretty_response)
+
+        elif(args.file is None):
+                with open("subdomains" + domain + ".json", "a+") as file:
+                    file.write(pretty_response)
+
     elif(args.xml == True):
-        for domain in args.domains:
-            response = client.get_raw(domain, output_format=Client.XML_FORMAT)
-            
+        response = client.get_raw(domain, output_format=Client.XML_FORMAT)
+        
+        if(args.file is not None):
+            args.file.write(response.text)
+
+        elif(args.file is None):
+                with open("subdomains" + domain + ".xml", "a+") as file:
+                    file.write(response.text)
+        
     else:
-        for domain in args.domains:
-            response = client.get(domain)
-            print("Subdomains for " + domain)
-            for record in response.result.records:
-                print("    " + record.domain)
+        response = client.get(domain)
+        print("Subdomains for " + domain)
+        
+        for record in response.result.records:
+            print("    " + record.domain)
+
+            if(args.file is not None):
+                args.file.write(record.domain + "\n")
+
+            elif(args.file is None):
+                with open("subdomains" + domain + ".txt", "a+") as file:
+                    file.write(record.domain + "\n")
 
 main()
