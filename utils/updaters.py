6feae8487a47eec3819526e7ab5d4d2e5facf18f
tuchaVshortah <nukenbai190104@gmail.com>
2022-11-04 15:32:09 +0600

    Define 2 updaters

--- utils/updaters.py
+++ utils/updaters.py
@@ -1,11 +1,42 @@
-import json
+from json import loads, dumps
+import xml.dom.minidom
 from subdomainslookup.models.response import Response as ApiResponse
 
-def updateJsonResponse(json_response: str):
+def updateJsonResponse(json_response: str, new_subdomains: list) -> str:
+    
+    response_data = loads(json_response)
+    records = response_data["records"]
+
+    old_subdomains = []
+    for record in records:
+        old_subdomains.append(record["domain"])
+
+    subdomains = []
+    subdomains.append(old_subdomains)
+    subdomains.append(new_subdomains)
+
+    unique_subdomains = list(set(subdomains))
+
+    for subdomain in unique_subdomains:
+        response_data["records"].append(
+            {
+                "domain": subdomain
+            }
+        )
+
+    json_response = dumps(response_data)
+
+    return json_response
+
+def updateXmlResponse(xml_response: str, new_subdomains: list) -> str:
     pass
 
-def updateXmlResponse(xml_response: str):
-    pass
+def updateRawResponse(raw_response: ApiResponse, new_subdomains: list) -> ApiResponse:
 
-def updateRawResponse(raw_response: ApiResponse):
-    pass
+    subdomains = raw_response.result.records
+    subdomains.append(new_subdomains)
+
+    unique_subdomains = list(set(subdomains))
+    raw_response.result.records = unique_subdomains
+
+    return raw_response
