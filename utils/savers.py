2e87b5e152ec3b8f62aeb947ca4f56b7100dd299
tuchaVshortah <nukenbai190104@gmail.com>
2022-11-12 20:21:17 +0600

    Remove not needed functions

--- utils/savers.py
+++ utils/savers.py
@@ -7,38 +7,3 @@ def saveResponseToFile(file, domain: str, reponse: str):
 def saveResponseToDefaultFile(domain: str, response: str, outputFormat: str):
     with open("subdomains." + domain + "." + outputFormat, "w") as file:
             file.write(response)
-
-def saveJsonResponse(file, domain, response: str):
-    if(file is not None):
-        file.write(response)
-
-    elif(file is None):
-        with open("subdomains." + domain + ".json", "w") as file:
-            file.write(response)
-
-def saveXmlResponse(file, domain, response: str):
-    if(file is not None):
-        file.write(response)
-
-    elif(file is None):
-        with open("subdomains." + domain + ".xml", "w") as file:
-            file.write(response)
-
-def saveTxtResponse(file, domain, response: ApiResponse or str):
-    
-    if(file is not None):
-        if(type(response) is ApiResponse):
-            for record in response.result.records:
-                file.write(record.domain + "\n")
-        
-        elif(type(response) is str):
-            file.write(response)
-        
-    elif(file is None):
-        with open("subdomains." + domain + ".txt", "w") as file:
-            if(type(response) is ApiResponse):
-                for record in response.result.records:
-                    file.write(record.domain + "\n")
-            
-            elif(type(response) is str):
-                file.write(response)
