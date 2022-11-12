bd0a341cb8acaf19e0af8e791b9c59cd5b46f42b
tuchaVshortah <nukenbai190104@gmail.com>
2022-11-12 20:20:20 +0600

    Add new functions and fix imports

--- utils/savers.py
+++ utils/savers.py
@@ -1,4 +1,12 @@
-from subdomainslookup.models.response import Response as ApiResponse
+from constants.outputformats import *
+
+def saveResponseToFile(file, domain: str, reponse: str):
+    if(file is not None):
+        file.write(response)
+
+def saveResponseToDefaultFile(domain: str, response: str, outputFormat: str):
+    with open("subdomains." + domain + "." + outputFormat, "w") as file:
+            file.write(response)
 
 def saveJsonResponse(file, domain, response: str):
     if(file is not None):
