256ffffb7d6ad3038fc8b8c3feb0a51ed2b35dbf
tuchaVshortah <nukenbai190104@gmail.com>
2022-11-16 14:10:11 +0600

    Add usage of the UrlscanApiRequester and rename a method to avoid ambiguity

--- wrappers/apiwrapper.py
+++ wrappers/apiwrapper.py
@@ -1,5 +1,6 @@
 from apis.whoisxmlapi import PuffClient, PuffApiRequester
 from apis.crtsh import CrtshApiRequester
+from apis.urlscan import UrlscanApiRequester
 from constants.outputformats import XML_FORMAT, JSON_FORMAT, RAW_FORMAT
 from threading import Thread
 from json import loads, dumps
@@ -14,6 +15,7 @@ class ApiWrapper():
     __puff_client = None
     __puff_api_requester = None
     __crtsh_api_requester = None
+    __urlscan_api_requester = None
     __outputFormat = None
     __boost = None
     __results = None
@@ -32,6 +34,7 @@ class ApiWrapper():
             self.__puff_api_requester = PuffApiRequester(self.__target, self.__outputFormat)
         
         self.__crtsh_api_requester = CrtshApiRequester(self.__target)
+        self.__urlscan_api_requester = UrlscanApiRequester(self.__target)
 
         
     
@@ -78,15 +81,23 @@ class ApiWrapper():
 
             puff_api_response = self.__puff_api_requester.post()
             crtsh_subdomains = self.__crtsh_api_requester.getSubdomains()
+            urlscan_subdomains = self.__urlscan_api_requester.getSubdomains()
 
-            return self.__updateResponse(puff_api_response, crtsh_subdomains)
+            new_data = self.__updateResponse(puff_api_response, crtsh_subdomains)
+            self.__updateDataObject(new_data, urlscan_subdomains)
+
+            return new_data
         
         elif(self.__puff_client is not None):
 
             puff_client_response = self.__puff_client.get_raw()
             crtsh_subdomains = self.__crtsh_api_requester.getSubdomains()
+            urlscan_subdomains = self.__urlscan_api_requester.getSubdomains()
 
-            return self.__updateResponse(puff_client_response, crtsh_subdomains)
+            new_data = self.__updateResponse(puff_client_response, crtsh_subdomains)
+            self.__updateDataObject(new_data, urlscan_subdomains)
+
+            return new_data
 
         
     def __fastTasks(self):
@@ -95,22 +106,31 @@ class ApiWrapper():
 
             self.__puff_api_requester.start()
             self.__crtsh_api_requester.start()
+            self.__urlscan_api_requester.start()
 
             puff_api_response = self.__puff_api_requester.join()
             crtsh_subdomains = self.__crtsh_api_requester.join()
+            urlscan_subdomains = self.__urlscan_api_requester.join()
 
-            return self.__updateResponse(puff_api_response, crtsh_subdomains)
+            new_data = self.__updateResponse(puff_api_response, crtsh_subdomains)
+            self.__updateDataObject(new_data, urlscan_subdomains)
+
+            return new_data
 
         elif(self.__puff_client is not None):
 
             self.__puff_client.start()
             self.__crtsh_api_requester.start()
+            self.__urlscan_api_requester.start()
 
             puff_client_response = self.__puff_client.join()
             crtsh_subdomains = self.__crtsh_api_requester.join()
+            urlscan_subdomains = self.__urlscan_api_requester.join()
 
-            return self.__updateResponse(puff_client_response, crtsh_subdomains)
+            new_data = self.__updateResponse(puff_client_response, crtsh_subdomains)
+            self.__updateDataObject(new_data, urlscan_subdomains)
 
+            return new_data
     
     def __updateResponse(self, response: str, new_subdomains) -> dict or Document or ApiResponse:
         
@@ -134,7 +154,7 @@ class ApiWrapper():
         return response_data
 
 
-    def __updateResponseObject(self, response_data: dict or Document or ApiResponse, new_subdomains):
+    def __updateDataObject(self, response_data: dict or Document or ApiResponse, new_subdomains):
 
         try:
 
