767c00bf7f31f40acbffdfaf95fb448c6d33baa0
tuchaVshortah <nukenbai190104@gmail.com>
2022-11-17 17:30:07 +0600

    Add support for AlienVault

---
+++ apis/alienvault.py
@@ -0,0 +1,58 @@
+import requests
+from json import loads
+from threading import Thread
+
+class AlienVaultApiRequester(Thread):
+
+    __domain = None 
+    __results = None
+
+    def __init__(self, domain:str = None):
+        Thread.__init__(self)
+
+        self.__domain = domain
+
+    def getSubdomains(self) -> list:
+
+        url = "https://otx.alienvault.com/api/v1/indicators/domain/{}/passive_dns".format(self.__domain)
+
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5195.102 Safari/537.36"
+        }
+
+        response = requests.get(url, headers)
+
+
+        subdomains = []
+        if(response.ok):
+            try:
+                
+                response = response.content.decode("utf-8")
+
+                response_data = loads(response)
+
+                records = response_data["passive_dns"]
+
+                for record in records:
+                    try:
+                        subdomain = record["hostname"]
+                        subdomains.append(subdomain)
+                    except:
+                        pass
+
+            except Exception as e:
+                print(e)
+
+        unique_subdomains = list(set(subdomains))
+
+        return unique_subdomains
+
+    def run(self):
+        self.__results = self.getSubdomains()
+
+    def join(self):
+        Thread.join(self)
+        return self.__results
+
+test = AlienVaultApiRequester("mechta.kz")
+print(test.getSubdomains())
