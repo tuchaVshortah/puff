cad6904d619220b8459156bbfa121a758f491e8a
tuchaVshortah <nukenbai190104@gmail.com>
2022-11-22 16:45:06 +0600

    Change parameter name to avoid confusion

--- apis/bases/Base.py
+++ apis/bases/Base.py
@@ -5,5 +5,5 @@ class Base():
     _response = None
     _results = None
 
-    def _checkSubdomain(self, domain: str) -> bool:
-        return domain.endswith(".{}".format(self._domainName))
+    def _checkSubdomain(self, subdomain: str) -> bool:
+        return subdomain.endswith(".{}".format(self._domainName))
